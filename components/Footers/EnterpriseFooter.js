478f8d06ab83b1cbf2960fc4af432ad3cb9c1b06
Jieff Cavalcanti <jieff.cavalcanti@gmail.com>
2023-11-23 12:21:23 -0300

    update initial

---
+++ components/Footers/EnterpriseFooter.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+// reactstrap components
+import {  Container, Row, Col } from "reactstrap";
+
+function EnterpriseFooter() {
+  return (
+    <Container>
+      <Row>
+        <Col className="text-center">
+          <p>@2023 análise de desempenho</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default EnterpriseFooter;
