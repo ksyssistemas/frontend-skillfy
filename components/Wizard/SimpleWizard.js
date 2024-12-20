0896f0a04859e52aaaa70d522e6a1fe501c0ea22
Bezalel Ribeiro <begmr206@gmail.com>
2024-12-20 14:09:22 +0000

    refactor: files required to update

--- components/Wizard/SimpleWizard.js
+++ components/Wizard/SimpleWizard.js
@@ -20,8 +20,7 @@ import {
     Progress
 } from "reactstrap";
 
-function SimpleWizard({ stepsNumber, currentStep, stepTitles }) {
-
+function SimpleWizard({ stepsNumber, currentStep, stepTitles, hasWizartStyleOfReviewCreation }) {
     const progressBarNumber = stepsNumber - 1;
 
     const steps = Array.from({ length: stepsNumber }).map((_, index) => {
@@ -36,8 +35,8 @@ function SimpleWizard({ stepsNumber, currentStep, stepTitles }) {
         }
     });
 
-    return (
-        <div className="pt-4 mb-0">
+    const wizardContent = () => {
+        return (
             <div className="d-flex flex-column align-items-center">
                 <nav className="mb-2 w-100">
                     <ol className="d-flex align-items-center justify-content-center list-unstyled">
@@ -45,35 +44,46 @@ function SimpleWizard({ stepsNumber, currentStep, stepTitles }) {
                             const title = stepTitles[index];
                             return (
                                 <React.Fragment key={index}>
-                                    <li className={`d-flex align-items-center justify-content-center ${step.status === 'isCurrent' ? 'font-weight-bold' : ''}`}  style={{zIndex: "10",}}>
+                                    <li className={`d-flex align-items-center justify-content-center ${step.status === 'isCurrent' ? 'font-weight-bold' : ''}`} style={{ zIndex: hasWizartStyleOfReviewCreation ? undefined : 10 }}>
                                         <a
                                             href="#"
                                             className="d-flex flex-column align-items-center justify-content-center p-1 text-decoration-none"
                                             style={{
-                                                color: step.status === 'isDone' ? '#BBDB35' : '#ffff'
+                                                color: hasWizartStyleOfReviewCreation
+                                                    ? (step.status === 'isDone' ? '#ff623f' : '#6c757d')
+                                                    : (step.status === 'isDone' ? '#BBDB35' : '#6c757d')
                                             }}
                                         >
                                             {step.status === 'isDone' ? (
                                                 <span
                                                     className="d-inline-flex align-items-center justify-content-center rounded-circle text-md"
                                                     style={{
-                                                        width: '4rem',
-                                                        height: '4rem',
-                                                        fontSize: '2rem',
-                                                        color: step.status === 'isDone' ? '#ffeeef' : '#57249F',
-                                                        backgroundColor: step.status === 'isDone' ? '#BBDB35' : '#e9ecef',
-                                                    }}>
+                                                        width: hasWizartStyleOfReviewCreation ? '2rem' : '4rem',
+                                                        height: hasWizartStyleOfReviewCreation ? '2rem' : '4rem',
+                                                        fontSize: hasWizartStyleOfReviewCreation ? undefined : '2rem',
+                                                        color: hasWizartStyleOfReviewCreation
+                                                            ? (step.status === 'isDone' ? '#ffeeef' : '#6c757d')
+                                                            : (step.status === 'isDone' ? '#ffeeef' : '#57249F'),
+                                                        backgroundColor: hasWizartStyleOfReviewCreation
+                                                            ? (step.status === 'isDone' ? '#ff623f' : '#e9ecef')
+                                                            : (step.status === 'isDone' ? '#BBDB35' : '#e9ecef'),
+                                                    }}
+                                                >
                                                     <i className="ni ni-check-bold"></i>
                                                 </span>
                                             ) : (
                                                 <span
                                                     className="d-inline-flex align-items-center justify-content-center rounded-circle text-md"
                                                     style={{
-                                                        width: '4rem',
-                                                        height: '4rem',
-                                                        fontSize: '2rem',
-                                                        color: step.status === 'isDone' ? '#ffeeef' : '#57249F',
-                                                        backgroundColor: step.status === 'isDone' ? '#BBDB35' : '#e9ecef',
+                                                        width: hasWizartStyleOfReviewCreation ? '2rem' : '4rem',
+                                                        height: hasWizartStyleOfReviewCreation ? '2rem' : '4rem',
+                                                        fontSize: hasWizartStyleOfReviewCreation ? undefined : '2rem',
+                                                        color: hasWizartStyleOfReviewCreation
+                                                            ? (step.status === 'isDone' ? '#ffeeef' : '#6c757d')
+                                                            : (step.status === 'isDone' ? '#ffeeef' : '#57249F'),
+                                                        backgroundColor: hasWizartStyleOfReviewCreation
+                                                            ? (step.status === 'isDone' ? '#ff623f' : '#e9ecef')
+                                                            : (step.status === 'isDone' ? '#BBDB35' : '#e9ecef'),
                                                     }}
                                                 >
                                                     {index + 1}
@@ -89,12 +99,16 @@ function SimpleWizard({ stepsNumber, currentStep, stepTitles }) {
                                         <div
                                             className="mb-4"
                                             style={{
-                                                width: '28rem',
-                                                zIndex: "10",
+                                                width: hasWizartStyleOfReviewCreation ? '6rem' : '28rem',
+                                                zIndex: hasWizartStyleOfReviewCreation ? undefined : 10
                                             }}
                                         >
                                             <Progress
-                                                color={`${step.status === 'isDone' ? 'green-sk' : 'light'}`}
+                                                color={
+                                                    hasWizartStyleOfReviewCreation
+                                                        ? (step.status === 'isDone' ? 'warning' : 'light')
+                                                        : (step.status === 'isDone' ? 'green-sk' : 'light')
+                                                }
                                                 className={`progress-xs mb-3 mb-md-0`}
                                                 max="100"
                                                 value="100"
@@ -107,7 +121,23 @@ function SimpleWizard({ stepsNumber, currentStep, stepTitles }) {
                     </ol>
                 </nav>
             </div>
-        </div>
+        );
+    };
+
+    return (
+        <>
+            {
+                hasWizartStyleOfReviewCreation ? (
+                    <Card className="pt-4 mb-0">
+                        {wizardContent()}
+                    </Card>
+                ) : (
+                    <div className="pt-4 mb-0">
+                        {wizardContent()}
+                    </div>
+                )
+            }
+        </>
     );
 }
 
@@ -115,6 +145,7 @@ SimpleWizard.propTypes = {
     stepsNumber: PropTypes.number,
     currentStep: PropTypes.number,
     stepTitles: PropTypes.array,
+    hasWizartStyleOfReviewCreation: PropTypes.bool,
 };
 
 export default SimpleWizard;
