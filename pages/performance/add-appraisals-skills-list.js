7f421e5130895af9677b4a6dd2c87748db25c16e
Bezalel Ribeiro <begmr206@gmail.com>
2025-01-14 17:21:55 +0000

    chore: ajustment of standard route

--- pages/performance/add-appraisals-skills-list.js
+++ pages/performance/add-appraisals-skills-list.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Container } from "reactstrap";
 import Performance from "../../layouts/Performance";
-import AppraisalCycleHeader from "../../components/Headers/PerformanceHeader/AppraisalCycleHeader";
+// import AppraisalCycleHeader from "../../components/Headers/PerformanceHeader/AppraisalCycleHeader";
 import AppraisalListHeader from "../../components/Headers/PerformanceHeader/AppraisalListHeader";
-import AppraisalCycleTable from "../../components/Tables/AppraisalTables/Appraisal/CycleAppraisal ";
+// import AppraisalCycleTable from "../../components/Tables/AppraisalTables/Appraisal/CycleAppraisal ";
 // import AppraisalsListTable from "../../components/Tables/AppraisalTables/Appraisal/AppraisalsListTable";
 import AppraisalsListTableCompetencies from "../../components/Tables/AppraisalTables/Appraisal/AppraisalsListTableCompetencies";
-import AddAppraisalCycleModal from "../../components/Modals/AppraisalModal/add-appraisal-cycle";
+// import AddAppraisalCycleModal from "../../components/Modals/AppraisalModal/add-appraisal-cycle";
 import AppraisalsSkillsRegister from "../../components/Forms/PerformanceForms/AppraisalsSkillsRegister";
 import { EvidencesContext } from '../../contexts/PerformanceContext/AppraisalEvidencesContext';
 
