ab329d46898bed6c4682b7f44341eb2f7a3e7243
Kauã Martins <kh-martins@hotmail.com>
2025-02-14 14:51:13 +0000

    chore: ajustment of manual participants

--- reducers/ReviewForms/ReviewParticipantSelectionFormReducer.js
+++ reducers/ReviewForms/ReviewParticipantSelectionFormReducer.js
@@ -38,6 +38,8 @@ export const initialState = {
         pairTagsInput: {},
         removedPairItems: {},
 
+        listPairEmployeeDataToReviewDataSelect: {},
+
         isHandPickedSelectionParticipantsToReview: false,
 
     },
@@ -245,7 +247,7 @@ export const formReducer = (state, action) => {
                     ...state.reviewParticipantsSelectionData,
                     listPairEmployeeDataToReview: {
                         ...state.reviewParticipantsSelectionData.listPairEmployeeDataToReview,
-                        ...action.payload,
+                        [action.payload.lideradoId]: action.payload.pairs,
                     },
                 },
             };
@@ -287,6 +289,29 @@ export const formReducer = (state, action) => {
                     isHandPickedSelectionParticipantsToReview: action.payload,
                 },
             };
+        case 'SET_LIST_EMPLOYEE_DATA_TO_SELECT':
+            return {
+                ...state,
+                reviewParticipantsSelectionData: {
+                    ...state.reviewParticipantsSelectionData,
+                    listPairEmployeeDataToReviewDataSelect: {
+                        ...state.reviewParticipantsSelectionData.listPairEmployeeDataToReviewDataSelect,
+                        ...action.payload,
+                    },
+                },
+            };
+        case 'SET_LIST_PAIR_EMPLOYEE_DATA_TO_SELECT':
+            return {
+                ...state,
+                reviewParticipantsSelectionData: {
+                    ...state.reviewParticipantsSelectionData,
+                    listPairEmployeeDataToReviewDataSelect: {
+                        ...state.reviewParticipantsSelectionData.listPairEmployeeDataToReviewDataSelect,
+                        [action.payload.lideradoId]: action.payload.pairs,
+                    },
+                },
+            };
+
         default:
             return state;
     }
